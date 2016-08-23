291ff0467d636c0d2bf343c35c181dfd13cd84c6
jackguo <guozhiqiang_bj@163.com>
2016-08-23 13:05:52 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/administrator/listviewtest/entry/News.java
+++ app/src/main/java/com/example/administrator/listviewtest/entry/News.java
@@ -6,4 +6,25 @@ package com.example.administrator.listviewtest.entry;
 public class News {
     int id;
     String title;
+
+    public News(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
