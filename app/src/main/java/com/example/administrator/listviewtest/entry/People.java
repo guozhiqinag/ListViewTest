a7ae017be72429db984f7056208dd61db350a0b9
jackguo <guozhiqiang_bj@163.com>
2016-08-23 13:31:28 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/administrator/listviewtest/entry/People.java
@@ -0,0 +1,14 @@
+package com.example.administrator.listviewtest.entry;
+
+/**
+ * Created by Administrator on 2016/8/23.
+ */
+public class People {
+    int id;
+    String name ;
+
+    public People(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
